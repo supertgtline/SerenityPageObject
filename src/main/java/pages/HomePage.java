879feb6d9c4995cda3dcce90fad91885a0c52a9f
giangthao <thaogiang.90@gmail.com>
2022-04-14 18:09:25 +0700

    56. Create the First Page Class

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import base.BasePage;
+
+public class HomePage extends BasePage {
+    String newCarMenu ="//div[contains(text(),'NEW CARS')]";
+    String findNewCar="//div[contains(text(),'Find New Cars')]";
+    String findUsedCars="";
+
+    public  void findNewCar(){
+
+    }
+    public  void findUsedCars(){
+
+    }
+    public void doLogin(){
+
+    }
+
+}
